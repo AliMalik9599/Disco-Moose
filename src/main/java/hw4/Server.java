d071133e40fbfe6608dba4c2fb9bf4bd9238a911
knewbold17 <katiesnewbold@gmail.com>
2020-10-08 23:00:04 -0400

    Completed tasks 1,  3, 4, of HW4

--- src/main/java/hw4/Server.java
+++ src/main/java/hw4/Server.java
@@ -2,8 +2,10 @@ package hw4;
 
 import hw4.exception.DaoException;
 import hw4.model.Author;
+import hw4.model.Book;
 import org.sql2o.Sql2o;
 import hw4.persistence.Sql2oAuthorDao;
+import hw4.persistence.Sql2oBookDao;
 import spark.ModelAndView;
 import java.util.HashMap;
 import java.util.Map;
@@ -19,7 +21,7 @@ public class Server {
         return new Sql2o(URI, USERNAME, PASSWORD);
     }
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         // set port number
         final int PORT_NUM = 7000;
         port(PORT_NUM);
@@ -28,59 +30,131 @@ public class Server {
 
         post("/", (req, res) -> {
             String username = req.queryParams("username");
+            String color = req.queryParams("color");
             res.cookie("username", username);
+            res.cookie("color", color);
             res.redirect("/");
             return null;
         });
 
         get("/", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
-            if (req.cookie("username") != null)
+            if (req.cookie("username") != null && req.cookie("color") != null) {
                 model.put("username", req.cookie("username"));
+                model.put("color", req.cookie("color"));
+            }
             res.status(200);
             res.type("text/html");
             return new ModelAndView(model, "public/templates/index.vm");
         }, new VelocityTemplateEngine());
 
         get("/authors", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            model.put("authors", new Sql2oAuthorDao(sql2o).listAll());
-            res.status(200);
-            res.type("text/html");
-            return new ModelAndView(model, "public/templates/authors.vm");
+            if (req.cookie("username") == null) {
+                res.redirect("/");
+                return null;
+            } else {
+                Map<String, Object> model = new HashMap<>();
+                model.put("authors", new Sql2oAuthorDao(sql2o).listAll());
+                res.status(200);
+                res.type("text/html");
+                return new ModelAndView(model, "public/templates/authors.vm");
+            }
         }, new VelocityTemplateEngine());
 
         get("/addauthor", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            res.status(200);
-            res.type("text/html");
-            return new ModelAndView(model, "public/templates/addauthor.vm");
+            if (req.cookie("username") == null) {
+                res.redirect("/");
+                return null;
+            } else {
+                Map<String, Object> model = new HashMap<>();
+                res.status(200);
+                res.type("text/html");
+                return new ModelAndView(model, "public/templates/addauthor.vm");
+            }
         }, new VelocityTemplateEngine());
 
         post("/addauthor", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            String name = req.queryParams("name");
-            int numOfBooks = Integer.parseInt(req.queryParams("numOfBooks"));
-            String nationality = req.queryParams("nationality");
-            Author author = new Author(name, numOfBooks, nationality);
-            try {
-                int id = new Sql2oAuthorDao(sql2o).add(author);
-                if (id > 0) {
-                    model.put("added", "true");
-                }
-                else {
+            if (req.cookie("username") == null) {
+                res.redirect("/");
+                return null;
+            } else {
+                Map<String, Object> model = new HashMap<>();
+                String name = req.queryParams("name");
+                int numOfBooks = Integer.parseInt(req.queryParams("numOfBooks"));
+                String nationality = req.queryParams("nationality");
+                Author author = new Author(name, numOfBooks, nationality);
+                try {
+                    int id = new Sql2oAuthorDao(sql2o).add(author);
+                    if (id > 0) {
+                        model.put("added", "true");
+                    } else {
+                        model.put("failedAdd", "true");
+                    }
+                } catch (DaoException ex) {
                     model.put("failedAdd", "true");
                 }
+                res.status(201);
+                res.type("text/html");
+                ModelAndView mdl = new ModelAndView(model, "public/templates/addauthor.vm");
+                return new VelocityTemplateEngine().render(mdl);
             }
-            catch (DaoException ex) {
-                model.put("failedAdd", "true");
-            }
-            res.status(201);
-            res.type("text/html");
-            ModelAndView mdl = new ModelAndView(model, "public/templates/addauthor.vm");
-            return new VelocityTemplateEngine().render(mdl);
         });
 
         /* TODO: add your new endpoints here! */
+        get("/books", (req, res) -> {
+            if (req.cookie("username") == null) {
+                res.redirect("/");
+                return null;
+            } else {
+                Map<String, Object> model = new HashMap<>();
+                model.put("books", new Sql2oBookDao(sql2o).listAll());
+                res.status(200);
+                res.type("text/html");
+                return new ModelAndView(model, "public/templates/books.vm");
+            }
+        }, new VelocityTemplateEngine());
+
+        get("/addbook", (req, res) -> {
+            if (req.cookie("username") == null) {
+                res.redirect("/");
+                return null;
+            } else {
+                Map<String, Object> model = new HashMap<>();
+                res.status(200);
+                res.type("text/html");
+                return new ModelAndView(model, "public/templates/addbook.vm");
+            }
+        }, new VelocityTemplateEngine());
+
+        post("/addbook", (req, res) -> {
+            if (req.cookie("username") == null) {
+                res.redirect("/");
+                return null;
+            } else {
+                Map<String, Object> model = new HashMap<>();
+                String title = req.queryParams("title");
+                String isbn = req.queryParams("isbn");
+                String publisher = req.queryParams("publisher");
+                int year = Integer.parseInt(req.queryParams("year"));
+                String author = req.queryParams("author");
+
+                Book book = new Book(title, isbn, publisher, year, 0);
+                try {
+                    int id = new Sql2oBookDao(sql2o).add(book);
+                    if (id > 0) {
+                        model.put("added", "true");
+                    } else {
+                        model.put("failedAdd", "true");
+                    }
+                } catch (DaoException ex) {
+                    model.put("failedAdd", "true");
+                }
+                model.put("added", "true");
+                res.status(201);
+                res.type("text/html");
+                ModelAndView mdl = new ModelAndView(model, "public/templates/addbook.vm");
+                return new VelocityTemplateEngine().render(mdl);
+            }
+        });
     }
 }
