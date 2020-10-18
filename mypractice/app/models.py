c831e653d29f5af2f9f0691adc671cd1f70d4344
AliMalik9599 <amalik36@jhu.edu>
2020-10-18 19:35:24 -0400

    Updated models to UML

--- mypractice/app/models.py
+++ mypractice/app/models.py
@@ -1,8 +1,25 @@
 from django.db import models
 
 
-class Deck(models.Model):
+class Course(models.Model):
 	name = models.CharField(max_length=10, default='', null=False)
+	description = models.CharField(max_length=10, default='', null=False)
+	num_skills = models.IntegerField(default=0)
+	num_cards = models.IntegerField(default=0)
+	course_id = models.CharField(max_length=50, default='', null=False)
+
+	#category = models.CharField(max_length=10, default='', null=False)
+
+	def __str__(self):
+		return '%s' % self.name
+
+class Skill(models.Model):
+	name = models.CharField(max_length=10, default='', null=False)
+	description = models.CharField(max_length=10, default='', null=False)
+	num_levels = models.IntegerField(default=0)
+	skill_id = models.CharField(max_length=50, default='', null=False)
+	course_id = models.CharField(max_length=50, default='', null=False)
+
 	#category = models.CharField(max_length=10, default='', null=False)
 
 	def __str__(self):
@@ -17,14 +34,18 @@ class Goal(models.Model):
 
 
 class Calendar(models.Model):
-	list_of_topics = models.ManyToManyField(Deck)
+	list_of_topics = models.ManyToManyField(Course)
 
 
 class User(models.Model):
 	name = models.CharField(max_length=50, default='', null=False)
-	deck_list = models.ManyToManyField(Deck)
-	#goal_list = models.ManyToManyField(Goal)
-	#calendar = models.ForeignKey(Calendar, on_delete=models.CASCADE)
+	username = models.CharField(max_length=50, default='', null=False)
+	user_id = models.CharField(max_length=50, default='', null=False)
+	password = models.CharField(max_length=50, default='', null=False)
+	topic_list = models.ManyToManyField(Course)
+	goal_list = models.ManyToManyField(Goal)
+
+	calendar = models.ForeignKey(Calendar, on_delete=models.CASCADE)
 
 
 class MyPractice(models.Model):
@@ -34,19 +55,36 @@ class MyPractice(models.Model):
 
 class Card(models.Model):
 	title = models.CharField(max_length=50, default='', null=False)
-	deck = models.ForeignKey(Deck, on_delete=models.CASCADE, null=True)
-	category = models.CharField(max_length=10, default='', null=False)
-	difficulty = models.IntegerField(default=0)
+	course_id = models.CharField(max_length=50, default='', null=False)
+	skill_id = models.CharField(max_length=50, default='', null=False)
+	level = models.IntegerField(default=0)
 	duration = models.IntegerField(default=0)
 	view_count = models.IntegerField(default=0)
 	content = models.CharField(max_length=50, default='', null=False)
+	complete = models.BooleanField(default=False)
+
+	#Kept these incase we need them later
+	""" deck = models.ForeignKey(Course, on_delete=models.CASCADE, null=True)
+	category = models.CharField(max_length=10, default='', null=False) """
 
 	def __str__(self):
 		return '%s in deck %s' % (self.title, self.deck.name)
 
 
 class DailySchedule(models.Model):
-	card_list = models.ManyToManyField(Deck)
+	card_list = models.ManyToManyField(Card)
+
+class CourseProgress(models.Model):
+	user_id = models.CharField(max_length=50, default='', null=False)
+	course_id = models.CharField(max_length=50, default='', null=False)
+	cards_completed = models.IntegerField(default=0)
+	course_cards = models.IntegerField(default=0)
+
+class SkillProgress(models.Model):
+	user_id = models.CharField(max_length=50, default='', null=False)
+	skill_id = models.CharField(max_length=50, default='', null=False)
+	current_level = models.IntegerField(default=0)
+	completed_status = models.BooleanField(blank=False, default=False)
 
 
 class Settings(models.Model):
