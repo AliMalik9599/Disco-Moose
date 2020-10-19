2a0c0380c5a7829bc4b8e3148ae2f0f1d7c3a73b
justg99 <justg99@gmail.com>
2020-10-19 16:59:48 -0400

    added course files

---
+++ mypractice/card-frontend/src/components/Course/Course.js
@@ -0,0 +1,23 @@
+import React, {Component} from "react";
+
+class Course extends Component {
+    render() {
+        return (
+            <div className="card task-wrapper" /* style="width: 18rem;" */>
+                <div className="card-header d-flex justify-content-between">
+                  <span>
+                    <strong>Name: </strong>{this.props.name}<br/>
+                    <strong>Description: </strong>{this.props.description}<br/>
+                    <strong>num_skills: </strong>{this.props.num_skills}<br/>
+                    <strong>num_cards: </strong>{this.props.num_cards}<br/>
+                  </span>
+                </div>
+                <div className="card-body">
+                    {this.props.description}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Card;
