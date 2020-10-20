9b851e5fff9939496337f222b570ae820a84c5ae
AliMalik9599 <amalik36@jhu.edu>
2020-10-20 09:25:15 -0400

    Added serializer and view for skills

--- mypractice/app/serializer.py
+++ mypractice/app/serializer.py
@@ -1,12 +1,20 @@
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User, Group, Skill
 from rest_framework import serializers
-from .models import Card, Course, User
+from .models import Card, Course, User, Skill
 
 
 class CourseSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Course
 		fields = ('id', 'name', 'description', 'num_skills', 'num_cards')
+
+
+class SkillSerializer(serializers.ModelSerializer):
+	course = serializers.StringRelatedField(many=False)
+
+	class Meta:
+		model = Skill
+		fields = ('id', 'name', 'description', 'num_levels', 'num_cards', 'course')
 
 
 class CardSerializer(serializers.ModelSerializer):
