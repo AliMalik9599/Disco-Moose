04201c1fe928e19bf02e459504ca1dc1816a7d6c
AliMalik9599 <amalik36@jhu.edu>
2020-10-20 11:32:33 -0400

    Minor changes to fields

--- mypractice/card-frontend/src/components/SkillList/Skill/Skill.js
+++ mypractice/card-frontend/src/components/SkillList/Skill/Skill.js
@@ -6,7 +6,7 @@ class Skill extends Component {
             <div className="skill task-wrapper">
                 <div className="skill-header d-flex justify-content-between">
                     <span>
-                        <strong>Name:</strong>{this.props.title} <br/>
+                        <strong>Name:</strong>{this.props.name} <br/>
                         <strong>Description: </strong>{this.props.description}<br/>
                         <strong>Number of Levels: </strong>{this.props.num_levels}<br/>
                         <strong>Number of Cards: </strong>{this.props.num_cards}<br/>
