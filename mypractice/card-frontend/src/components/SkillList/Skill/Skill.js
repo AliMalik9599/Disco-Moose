e6192c9de26c28f9ffc4d98b63928852c95b91bd
justg99 <justg99@gmail.com>
2020-11-04 21:54:15 -0500

    added material-ui designs to the skillList page, aligning content and adding button-like view.

--- mypractice/card-frontend/src/components/SkillList/Skill/Skill.js
+++ mypractice/card-frontend/src/components/SkillList/Skill/Skill.js
@@ -4,12 +4,21 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormGroup from '@material-ui/core/FormGroup';
 import Checkbox from '@material-ui/core/Checkbox';
-import {red} from "@material-ui/core/colors";
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
 const useStyles = makeStyles((theme) => ({
     root: {
         maxWidth: 345,
     },
+    box: {
+        border: 1,
+        backgroundColor: '#D2E4EE',
+        padding: '5% 25%',
+        textAlign: 'center',
+        margin: 5,
+        borderRadius: '10px',
+    }
 }));
 
 export default function Skill(props) {
@@ -29,38 +38,15 @@ export default function Skill(props) {
 
     const classes = useStyles();
     return (
-        <FormControl className="skill task-wrapper">
-            <FormGroup>
-                <FormControlLabel
-                    control={<Checkbox checked={ticked} onClick={(e) => props.skillUpdate(e, props.id)} onChange={handleChange} name="checked"/>}
-                    label={props.name}
-                />
-            </FormGroup>
-        </FormControl>
+        <Box className={classes.box}>
+            <FormControl className="skill task-wrapper">
+                <FormGroup>
+                    <FormControlLabel
+                        control={<Checkbox checked={ticked} onClick={(e) => props.skillUpdate(e, props.id)} onChange={handleChange} name="checked"/>}
+                        label={props.name}
+                    />
+                </FormGroup>
+            </FormControl>
+        </Box>
     );
 }
-
-
-
-//
-// class Skill extends Component {
-//     render() {
-//         return(
-//             <div className="skill task-wrapper" onClick={(e) => this.props.skillUpdate(e, this.props.id)}>
-//                 <div className="skill-header d-flex justify-content-between">
-//                     <span>
-//                         <strong>Name:</strong>{this.props.name} <br/>
-//                         <strong>Description: </strong>{this.props.description}<br/>
-//                         <strong>Number of Levels: </strong>{this.props.num_levels}<br/>
-//                         <strong>Number of Cards: </strong>{this.props.num_cards}<br/>
-//                         <strong>Course: </strong>{this.props.course}<br/>
-//                     </span>
-//                 </div>
-//                 <div className="skill-body">
-//                     {this.props.description}
-//                 </div>
-//             </div>
-//         );
-//     }
-//
-// }
