1b8475b8ef69d1c73fdec501f31328a9adfdbdb1
justg99 <justg99@gmail.com>
2020-11-17 19:49:52 -0500

    committing build files

---
+++ mypractice/card-frontend/build/precache-manifest.657edaaf2a60107856c78ea40d73f73d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f6c45993cd63fc02d327ce219a6be93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "987089a739f556357455",
+    "url": "/static/css/main.791e1333.chunk.css"
+  },
+  {
+    "revision": "1390cd0ca6c69c391dc1",
+    "url": "/static/js/2.5bdadc75.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.5bdadc75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "987089a739f556357455",
+    "url": "/static/js/main.09938c3a.chunk.js"
+  },
+  {
+    "revision": "bd7feb6b09923e298f75",
+    "url": "/static/js/runtime-main.14f6456e.js"
+  },
+  {
+    "revision": "bd730b9dfe62d15ffd359ca51acc55c5",
+    "url": "/static/media/bulb-logo.bd730b9d.png"
+  },
+  {
+    "revision": "0fd658ff21d7f18d5d11cce487064fde",
+    "url": "/static/media/loginAnimation.0fd658ff.gif"
+  }
+]);
