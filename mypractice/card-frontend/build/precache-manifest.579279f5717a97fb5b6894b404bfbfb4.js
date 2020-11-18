a0d06e3b14fe903f6de5dd9872d023852ed1d812
justg99 <justg99@gmail.com>
2020-11-17 22:42:50 -0500

    reverted public url

---
+++ mypractice/card-frontend/build/precache-manifest.579279f5717a97fb5b6894b404bfbfb4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b007bef5487efa90d28f8927e4dea458",
+    "url": "/index.html"
+  },
+  {
+    "revision": "999f5fcbec353b2dde30",
+    "url": "/static/css/main.791e1333.chunk.css"
+  },
+  {
+    "revision": "b4f97ceb9d56b49ae80c",
+    "url": "/static/js/2.e686ea36.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.e686ea36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "999f5fcbec353b2dde30",
+    "url": "/static/js/main.9120fe19.chunk.js"
+  },
+  {
+    "revision": "bd7feb6b09923e298f75",
+    "url": "/static/js/runtime-main.14f6456e.js"
+  },
+  {
+    "revision": "bd730b9dfe62d15ffd359ca51acc55c5",
+    "url": "/static/media/bulb-logo.bd730b9d.png"
+  },
+  {
+    "revision": "0fd658ff21d7f18d5d11cce487064fde",
+    "url": "/static/media/loginAnimation.0fd658ff.gif"
+  }
+]);
