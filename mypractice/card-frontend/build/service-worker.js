550f20e54c367cd2eede85fec0c9c51bb1564076
justg99 <justg99@gmail.com>
2020-12-06 16:27:22 -0500

    build for landing

--- mypractice/card-frontend/build/service-worker.js
+++ mypractice/card-frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a7299015f885ccefa15892e398ccfede.js"
+  "/precache-manifest.6ae2fdb315ecf48c684a4d16f3301885.js"
 );
 
 self.addEventListener('message', (event) => {
