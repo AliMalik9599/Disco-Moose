ca0c78e504cf23ed590ba0468e3472deb7fc00b6
knewbol1 <knewbol1@jhu.edu>
2020-12-20 09:14:15 -0800

    deleted code that was messing up user persistence from App.js

--- mypractice/card-frontend/src/App.js
+++ mypractice/card-frontend/src/App.js
@@ -2,9 +2,9 @@ import React, {Component} from 'react';
 import Layout from "./hoc/Layout/Layout";
 import './App.css';
 
-window.onbeforeunload = function() {
-    localStorage.clear();
-};
+// window.onbeforeunload = function() {
+//     localStorage.clear();
+// };
 
 class App extends Component {
     render() {
